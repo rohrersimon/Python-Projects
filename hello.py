7c6c907d02c6f58368c3b8e068a4da5eaea8f32c
Simon Rohrer <simon.rohrer@live.com>
2024-02-15 13:47:28 +0000

    c

--- hello.py
+++
@@ -1,8 +0,0 @@
-def main():
-    name = input("What's your name? ")
-    hello(name)
-
-def hello(to = "World"):
-    print("hello,", to)
-
-main()
--- indoor.py
+++
@@ -1,5 +0,0 @@
-userInput = input("Write a phrase you want to whisper: ")
-
-userInput.lower()
-
-print(userInput)
