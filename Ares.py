558610ab95619c61a85e76b259586478a0d4cf1a
Simon Rohrer <simon.rohrer@live.com>
2024-10-11 10:48:38 +0100

    Update Ares.py

--- Ares.py
+++ Ares.py
@@ -1,13 +1,6 @@
-def __init__(self):
-    self.numbers = [3, 343, 2, 87, 4.56, 867, 309, 667, 1494, 84, 9544, 53, 2223]
+with open('newFile.txt', 'w') as file:
+    file.write('Hello, world!')
 
-def print_even(number):
-	if number % 2 == 0:
-		print("Number " + str(number) + " is an even number")
-	else:
-		print(f"Number " + str(number) + " is an odd number")
-
-numberList = [2, 4, 33454, 445, 5985, 354]
-
-for number in numberList:
-	print_even(number)
+with open('newFile.txt', 'r') as file:
+    content = file.read()
+    print(content)
